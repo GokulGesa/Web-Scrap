b15da543fd42a168a43bed309b6b390b81a03618
GokulGesa <gokulsouthern@gmail.com>
2021-12-17 12:30:38 +0530

    first commit

--- scrape_amazon.py
+++ scrape_amazon.py
@@ -20,17 +20,14 @@ rating_class = "a-icon-alt"
 no_of_rat_class = 'a-size-base'
 main_page_url_class ="a-link-normal a-text-normal"
 
-#scrape the product details
+
 product_names = soup.find_all("span", {"class":product_name_class})
 product_prices = soup.find_all("span",{"class":actual_price_class})
 ratings = soup.find_all("span",{"class":rating_class})
 no_of_ratings = soup.find_all('span',{'class':no_of_rat_class})
 main_page_urls = soup.find_all("a",{'class': main_page_url_class})
 
-print(product_names[0].text.strip())
-print(product_prices[0].text.strip())
-print(ratings[0].text.strip())
-# print(no_of_ratings[0].content)
+
 print('amazon.in'+ main_page_urls[0].get('href').strip())
 
 print(len(product_names))
@@ -43,7 +40,7 @@ product_prices_df = []
 ratings_df = []
 main_page_urls_df = []
 
-#make a dataframe
+
 for i in range(len(product_names)):
 
 	product_names_df.append(product_names[i].text.strip())
